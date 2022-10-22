625aeca9832a40470ff2d9a0fd6411b920d5a29c
Anthony Kuregian <AnthonyKuregian@yahoo.com>
2022-10-21 23:15:23 -0700

    Configured OpenCV to work inside project with CMAKE

--- ImageClassifier/Source/main.cpp
+++ ImageClassifier/Source/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main() {
     std::cout << "Hello\n";
-    std::cin.get();
+    cv::Mat Image = cv::imread("C:/Users/Antho/OneDrive/Desktop/test.jpg", cv::IMREAD_COLOR);
+    cv::imshow("Image Window", Image);
+    int k = cv::waitKey(0);
+    if(k == 'c') { cv::destroyAllWindows(); }
     return 0;
+
 }
